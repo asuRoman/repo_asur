525952245a473eecf3100a2dc529f882eea1bfe2
asuRoman <romkakalf@gmail.com>
2026-07-22 15:14:56 +0300

    Update web_server.sh

--- web_server.sh
+++ web_server.sh
@@ -1,5 +1,7 @@
-apt install nginx
-apt install apache2
+#!/bin/bash
+apt update
+apt install -y nginx
+apt install -y apache2
 cp --attributes-only /etc/apache2/ports.conf /home/asur/repo_asur/ports.conf && mv /home/asur/repo_asur/ports.conf /etc/apache2/ports.conf
 apachectl -t
 service apache2 start
@@ -18,7 +20,7 @@ cp --attributes-only /var/www/html1/index.html /home/asur/repo_asur/html/html1/i
 cp --attributes-only /var/www/html2/index.html /home/asur/repo_asur/html/html2/index.html && mv /home/asur/repo_asur/html/html2/index.html /var/www/html2/index.html
 
 sudo service apache2 reload
-apt install prometheus-node-exporter
+apt install -y prometheus-node-exporter
 dpkg -i /home/asur/filebeat_8.17.1_amd64-224190-a5f894.deb
 cp --attributes-only /etc/filebeat/filebeat.yml /home/asur/repo_asur/filebeat.yml && mv /home/asur/repo_asur/filebeat.yml /etc/filebeat/filebeat.yml
 sudo systemctl restart filebeat
