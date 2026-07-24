2b0a7ecb266a908e8df688f7d611ca3b0de5cd66
asuRoman <romkakalf@gmail.com>
2026-07-24 10:43:08 +0300

    Update mysql_server.sh

--- mysql_server.sh
+++ mysql_server.sh
@@ -6,7 +6,7 @@ apt update
 apt install mysql-server-8.0
 systemctl start mysql
 cp -f /home/asur/repo_asur/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
-systemctl start mysql
+systemctl restart mysql
 sudo mysql
 CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY '12131415';
 # выдать права на работу с репликацией
