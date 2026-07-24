1f42ee6f52f167eb68dd62cb4e95af4945a4c517
asuRoman <romkakalf@gmail.com>
2026-07-24 12:15:32 +0300

    Update mysql_server.sh

--- mysql_server.sh
+++ mysql_server.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-hostnamectl set-hostname mysql-master
-systemctl reboot
-apt update
-apt install mysql-server-8.0
-systemctl start mysql
+#hostnamectl set-hostname mysql-master
+#systemctl reboot
+#apt update
+echo "Выполняется установка mysql"
+#apt install mysql-server-8.0
+echo "Установка mysql успешно выполнилась"
+#systemctl start mysql
+echo "Выполняется настройка конфигурационных файлов mysql"
 cp -f /home/asur/repo_asur/mysql/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl restart mysql
-mysql
-CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY '12131415';
+#mysql
+echo "Выполняется создание пользователя для подключения с репликации"
+#CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY '12131415';
 # выдать права на работу с репликацией
-GRANT REPLICATION SLAVE ON *.* TO repl@'%';
-exit
+echo "Выдаются права на работу с репликации"
+#GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+#exit
