5bf9ecaa36f0056dc75556816b52c6621adfc2cd
asuRoman <romkakalf@gmail.com>
2026-07-24 10:47:31 +0300

    Update mysql_repl.sh

--- mysql_repl.sh
+++ mysql_repl.sh
@@ -7,8 +7,8 @@ apt install mysql-server-8.0
 systemctl start mysql
 cp -f /home/asur/repo_asur/repl/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 cp -f /home/asur/repo_asur/repl/.my.cnf /home/asur
-service mysql restart
-sudo mysql
+systemctl restart mysql
+mysql
 STOP REPLICA;
 CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.1.113', SOURCE_USER='repl', SOURCE_PASSWORD='12131415', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
 START REPLICA;
