74c11c70eb531be10d16376e47b5ed96af119b44
asuRoman <romkakalf@gmail.com>
2026-07-24 10:50:52 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,7 +4,7 @@
 #apt install -y adduser libfontconfig1 musl
 #wget https://dl.grafana.com/oss/release/grafana_10.0.3_amd64.deb
 #dpkg -i grafana_10.0.3_amd64.deb
-cp -f /home/asur/repo_asur/prometheus.yml /etc/prometheus/prometheus.yml
+cp -f /home/asur/repo_asur/mon/prometheus.yml /etc/prometheus/prometheus.yml
 #systemctl daemon-reload
 #systemctl enable --now grafana-server.service
 systemctl restart grafana-server.service
