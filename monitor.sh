46a57b753916de684dc0bb464df7100b33b803e1
asuRoman <romkakalf@gmail.com>
2026-07-24 12:20:47 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+echo "Выполняется установка prometheus"
 #apt install prometheus
+echo "Выполняется установка grafana"
 #apt install -y adduser libfontconfig1 musl
 #wget https://dl.grafana.com/oss/release/grafana_10.0.3_amd64.deb
 #dpkg -i grafana_10.0.3_amd64.deb
+echo "Выполняется настройка конфигурационных файлов prometheus"
 cp -f /home/asur/repo_asur/mon/prometheus.yml /etc/prometheus/prometheus.yml
 #systemctl daemon-reload
 #systemctl enable --now grafana-server.service
 systemctl restart grafana-server.service
 systemctl restart prometheus
+echo "Скрипт выполнился успешно"
