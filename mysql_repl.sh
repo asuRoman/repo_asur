85e3d5a0eb30b450cbe14f4e3cbba3047fad801c
asuRoman <romkakalf@gmail.com>
2026-07-29 11:47:08 +0300

    Update mysql_repl.sh

--- mysql_repl.sh
+++ mysql_repl.sh
@@ -12,10 +12,12 @@ cp -f /home/asur/repo_asur/repl/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "Создается файл с авторизационными данными для создания бэкапа"
 cp -f /home/asur/repo_asur/repl/.my.cnf /home/asur
 systemctl restart mysql
-#mysql
+#mysql <<EOF
 #STOP REPLICA;
 echo "Выполняется запуск репликации"
 #CHANGE REPLICATION SOURCE TO SOURCE_HOST='192.168.1.113', SOURCE_USER='repl', SOURCE_PASSWORD='12131415', SOURCE_AUTO_POSITION = 1, GET_SOURCE_PUBLIC_KEY = 1;
 #START REPLICA;
 echo "Вывод состояния репликации"
 show replica status\G;
+EOF
+exit
